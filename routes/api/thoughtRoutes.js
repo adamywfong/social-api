df49c1c3d3f6f065b2f1ac73c277b79b338cb169
afong <adam.y.w.fong@gmail.com>
2023-11-06 21:14:03 -0800

    updated routes folder

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThoughts,
+  updateThought,
+  deleteThought,
+  addReaction,
+  deleteReaction
+} = require('../../controllers/thoughtController');
+
+router.route('/').get(getThoughts).post(createThoughts);
+
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
+router.route('/:thoughtId/reactions').post(addReaction);
+
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
+
+module.exports = router;
