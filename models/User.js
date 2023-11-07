93d3cd3ca8f227d3b169f9ee1e7294f47f6b87e6
afong <adam.y.w.fong@gmail.com>
2023-11-06 21:33:41 -0800

    updated User model

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please fill a valid email address']
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'user',
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    }
+  }
+);
+
+// Create a virtual property `commentCount` that gets the amount of comments per post
+userSchema.virtual('friendCount').get(function () {
+  return this.friends.length;
+});
+
+// Initialize our User model
+const User = model('user', userSchema);
+
+module.exports = User;
